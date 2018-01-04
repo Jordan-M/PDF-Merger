b849a50d66cf1eb05baa69a705f700896f9f8a3f
Jordan Mryyan <jmryyan@ksu.edu>
2018-01-03 23:26:48 -0600

    Finished base program

---
+++ PdfMerger/UserInterface.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace PdfMerger
+{
+    partial class UserInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxMergeButton = new System.Windows.Forms.Button();
+            this.uxSourceBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.uxProgressBar = new System.Windows.Forms.ProgressBar();
+            this.uxSourceButton = new System.Windows.Forms.Button();
+            this.uxDestButton = new System.Windows.Forms.Button();
+            this.uxSource = new System.Windows.Forms.TextBox();
+            this.uxDest = new System.Windows.Forms.TextBox();
+            this.uxDestBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.uxProgressLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // uxMergeButton
+            // 
+            this.uxMergeButton.Location = new System.Drawing.Point(12, 126);
+            this.uxMergeButton.Name = "uxMergeButton";
+            this.uxMergeButton.Size = new System.Drawing.Size(260, 23);
+            this.uxMergeButton.TabIndex = 0;
+            this.uxMergeButton.Text = "Merge";
+            this.uxMergeButton.UseVisualStyleBackColor = true;
+            this.uxMergeButton.Click += new System.EventHandler(this.uxMergeButton_ClickAsync);
+            // 
+            // uxProgressBar
+            // 
+            this.uxProgressBar.Location = new System.Drawing.Point(13, 70);
+            this.uxProgressBar.Name = "uxProgressBar";
+            this.uxProgressBar.Size = new System.Drawing.Size(259, 23);
+            this.uxProgressBar.TabIndex = 1;
+            // 
+            // uxSourceButton
+            // 
+            this.uxSourceButton.Location = new System.Drawing.Point(240, 12);
+            this.uxSourceButton.Name = "uxSourceButton";
+            this.uxSourceButton.Size = new System.Drawing.Size(32, 23);
+            this.uxSourceButton.TabIndex = 2;
+            this.uxSourceButton.Text = "...";
+            this.uxSourceButton.UseVisualStyleBackColor = true;
+            this.uxSourceButton.Click += new System.EventHandler(this.uxSourceButton_Click);
+            // 
+            // uxDestButton
+            // 
+            this.uxDestButton.Location = new System.Drawing.Point(240, 41);
+            this.uxDestButton.Name = "uxDestButton";
+            this.uxDestButton.Size = new System.Drawing.Size(32, 23);
+            this.uxDestButton.TabIndex = 3;
+            this.uxDestButton.Text = "...";
+            this.uxDestButton.UseVisualStyleBackColor = true;
+            this.uxDestButton.Click += new System.EventHandler(this.uxDestButton_Click);
+            // 
+            // uxSource
+            // 
+            this.uxSource.Location = new System.Drawing.Point(13, 12);
+            this.uxSource.Name = "uxSource";
+            this.uxSource.ReadOnly = true;
+            this.uxSource.Size = new System.Drawing.Size(221, 20);
+            this.uxSource.TabIndex = 4;
+            // 
+            // uxDest
+            // 
+            this.uxDest.Location = new System.Drawing.Point(13, 44);
+            this.uxDest.Name = "uxDest";
+            this.uxDest.ReadOnly = true;
+            this.uxDest.Size = new System.Drawing.Size(221, 20);
+            this.uxDest.TabIndex = 5;
+            // 
+            // uxProgressLabel
+            // 
+            this.uxProgressLabel.AutoSize = true;
+            this.uxProgressLabel.Location = new System.Drawing.Point(124, 96);
+            this.uxProgressLabel.Name = "uxProgressLabel";
+            this.uxProgressLabel.Size = new System.Drawing.Size(24, 13);
+            this.uxProgressLabel.TabIndex = 6;
+            this.uxProgressLabel.Text = "0/0";
+            // 
+            // UserInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.Controls.Add(this.uxProgressLabel);
+            this.Controls.Add(this.uxDest);
+            this.Controls.Add(this.uxSource);
+            this.Controls.Add(this.uxDestButton);
+            this.Controls.Add(this.uxSourceButton);
+            this.Controls.Add(this.uxProgressBar);
+            this.Controls.Add(this.uxMergeButton);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(300, 200);
+            this.MinimumSize = new System.Drawing.Size(300, 200);
+            this.Name = "UserInterface";
+            this.Text = "Pdf Merge Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uxMergeButton;
+        private System.Windows.Forms.FolderBrowserDialog uxSourceBrowser;
+        private System.Windows.Forms.ProgressBar uxProgressBar;
+        private System.Windows.Forms.Button uxSourceButton;
+        private System.Windows.Forms.Button uxDestButton;
+        private System.Windows.Forms.TextBox uxSource;
+        private System.Windows.Forms.TextBox uxDest;
+        private System.Windows.Forms.FolderBrowserDialog uxDestBrowser;
+        private System.Windows.Forms.Label uxProgressLabel;
+    }
+}
+
---
+++ PdfMerger/UserInterface.cs
@@ -0,0 +1,174 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PdfMerger
+{
+    public partial class UserInterface : Form
+    {
+        public UserInterface()
+        {
+            InitializeComponent();
+        }
+
+        private async void uxMergeButton_ClickAsync(object sender, EventArgs e)
+        {
+            if (uxSource.Text != String.Empty && uxDest.Text != String.Empty)
+            {
+                ResetUI();
+
+                DirectoryInfo dir = new DirectoryInfo(uxSource.Text);
+                Task<bool> test = await Task.Factory.StartNew(() => MergeFolder(dir, uxDest.Text));
+
+                if (test.Result)
+                {
+                    RemoveEmptyFolders(Path.Combine(uxDest.Text, dir.Name));
+                    uxProgressLabel.Text = "Cleaning folders...";
+                    MessageBox.Show("Finished merging PDFs!");
+                }
+
+                ResetUI();
+            }
+        }
+
+        private void uxSourceButton_Click(object sender, EventArgs e)
+        {
+            if (uxSourceBrowser.ShowDialog() == DialogResult.OK)
+            {
+                uxSource.Text = uxSourceBrowser.SelectedPath;
+            }
+        }
+
+        private void uxDestButton_Click(object sender, EventArgs e)
+        {
+            if (uxDestBrowser.ShowDialog() == DialogResult.OK)
+            {
+                uxDest.Text = uxDestBrowser.SelectedPath;
+            }
+        }
+
+        private void ResetUI()
+        {
+            ToggleUIButtons();
+            ResetProgress();
+        }
+
+        private void ResetProgress()
+        {
+            uxProgressBar.Value = 0;
+            uxProgressBar.Maximum = CalculateNumFiles(uxSource.Text, true);
+            uxProgressLabel.Text = "0/" + uxProgressBar.Maximum;
+        }
+
+        private void ToggleUIButtons()
+        {
+            uxSourceButton.Enabled = !uxSourceButton.Enabled;
+            uxDestButton.Enabled = !uxDestButton.Enabled;
+            uxMergeButton.Enabled = !uxMergeButton.Enabled;
+        }
+
+        public static int CalculateNumFiles(string location, bool searchSubfolders)
+        {
+            SearchOption option = (searchSubfolders) ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            return Directory.GetFiles(location, "*.*", option).Length;
+        }
+
+        private void RemoveEmptyFolders(string root)
+        {
+            foreach (var directory in Directory.GetDirectories(root))
+            {
+                RemoveEmptyFolders(directory);
+                if (Directory.GetFiles(directory).Length == 0 && Directory.GetDirectories(directory).Length == 0)
+                {
+                    Directory.Delete(directory, false);
+                }
+            }
+        }
+
+        private async Task<bool> MergeFolder(DirectoryInfo root, string savePath)
+        {
+            savePath = Path.Combine(savePath, root.Name);
+
+            if (Directory.Exists(savePath))
+            {
+                MessageBox.Show("Folder Conflict! Please choose a diffrent save location.");
+                return false;
+            }
+
+            Directory.CreateDirectory(savePath);
+
+            FileInfo[] files = null;
+            DirectoryInfo[] subDirs = null;
+
+            try
+            {
+                files = root.GetFiles("*.pdf");
+            }
+            catch(UnauthorizedAccessException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            if (files != null)
+            {
+                if (files.Count() > 0)
+                {
+                    PdfDocument pdf = MergePdfs(files);
+
+                    string realSavePath = savePath.Substring(0, savePath.LastIndexOf('\\'));
+
+                    pdf.Save(Path.Combine(realSavePath, root.Name + ".pdf"));
+                }
+
+                subDirs = root.GetDirectories();
+
+                foreach (DirectoryInfo dirInfo in subDirs)
+                {
+                    await MergeFolder(dirInfo, savePath);
+                }
+            }
+
+            return true;
+        }
+
+        private PdfDocument MergePdfs(FileInfo[] pdfFilePaths)
+        {
+            // Create the PDF 
+            PdfDocument mergedPDF = new PdfDocument();
+
+            foreach (FileInfo pdf in pdfFilePaths)
+            {
+                PdfDocument inputDoc = PdfReader.Open(pdf.FullName, PdfDocumentOpenMode.Import);
+
+                // We shouldn't need this because all of the pages are singles, but I am keeping it for more portability
+                for (int i = 0; i < inputDoc.PageCount; i++)
+                {
+                    PdfPage page = inputDoc.Pages[i];
+                    mergedPDF.AddPage(page);
+
+                    if (InvokeRequired)
+                    {
+                        BeginInvoke(new Action(() =>
+                        {
+                            uxProgressBar.Value++;
+                            uxProgressLabel.Text = uxProgressBar.Value + "/" + uxProgressBar.Maximum;
+                        }));
+                    }
+                }
+            }
+
+            return mergedPDF;
+        }
+
+
+    }
+}
