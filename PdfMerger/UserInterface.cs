4ed00d8bd7490b85e24e659c6b72a9e5be4df678
Jordan Mryyan <jmryyan@ksu.edu>
2018-01-08 10:07:56 -0600

    Testing update feature.

--- PdfMerger/UserInterface.cs
+++ PdfMerger/UserInterface.cs
@@ -16,6 +16,7 @@ namespace PdfMerger
         public UserInterface()
         {
             InitializeComponent();
+            MessageBox.Show("Updated!");
         }
 
         private void uxSourceButton_Click(object sender, EventArgs e)
