13b574d70bc41f0609ed158f8fe2a5ee0cef74cc
Jordan Mryyan <jmryyan@ksu.edu>
2018-01-08 14:50:43 -0600

    Added source and dest labels to UI

--- PdfMerger/UserInterface.Designer.cs
+++ PdfMerger/UserInterface.Designer.cs
@@ -38,13 +38,15 @@
             this.uxDestBrowser = new System.Windows.Forms.FolderBrowserDialog();
             this.uxProgressLabel = new System.Windows.Forms.Label();
             this.uxUseFolderSchema = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // uxMergeButton
             // 
             this.uxMergeButton.Location = new System.Drawing.Point(138, 126);
             this.uxMergeButton.Name = "uxMergeButton";
-            this.uxMergeButton.Size = new System.Drawing.Size(134, 23);
+            this.uxMergeButton.Size = new System.Drawing.Size(185, 23);
             this.uxMergeButton.TabIndex = 0;
             this.uxMergeButton.Text = "Merge";
             this.uxMergeButton.UseVisualStyleBackColor = true;
@@ -54,12 +56,12 @@
             // 
             this.uxProgressBar.Location = new System.Drawing.Point(13, 70);
             this.uxProgressBar.Name = "uxProgressBar";
-            this.uxProgressBar.Size = new System.Drawing.Size(259, 23);
+            this.uxProgressBar.Size = new System.Drawing.Size(310, 23);
             this.uxProgressBar.TabIndex = 1;
             // 
             // uxSourceButton
             // 
-            this.uxSourceButton.Location = new System.Drawing.Point(240, 12);
+            this.uxSourceButton.Location = new System.Drawing.Point(291, 12);
             this.uxSourceButton.Name = "uxSourceButton";
             this.uxSourceButton.Size = new System.Drawing.Size(32, 23);
             this.uxSourceButton.TabIndex = 2;
@@ -69,7 +71,7 @@
             // 
             // uxDestButton
             // 
-            this.uxDestButton.Location = new System.Drawing.Point(240, 41);
+            this.uxDestButton.Location = new System.Drawing.Point(291, 41);
             this.uxDestButton.Name = "uxDestButton";
             this.uxDestButton.Size = new System.Drawing.Size(32, 23);
             this.uxDestButton.TabIndex = 3;
@@ -79,24 +81,24 @@
             // 
             // uxSource
             // 
-            this.uxSource.Location = new System.Drawing.Point(13, 12);
+            this.uxSource.Location = new System.Drawing.Point(79, 12);
             this.uxSource.Name = "uxSource";
             this.uxSource.ReadOnly = true;
-            this.uxSource.Size = new System.Drawing.Size(221, 20);
+            this.uxSource.Size = new System.Drawing.Size(206, 20);
             this.uxSource.TabIndex = 4;
             // 
             // uxDest
             // 
-            this.uxDest.Location = new System.Drawing.Point(13, 44);
+            this.uxDest.Location = new System.Drawing.Point(79, 44);
             this.uxDest.Name = "uxDest";
             this.uxDest.ReadOnly = true;
-            this.uxDest.Size = new System.Drawing.Size(221, 20);
+            this.uxDest.Size = new System.Drawing.Size(206, 20);
             this.uxDest.TabIndex = 5;
             // 
             // uxProgressLabel
             // 
             this.uxProgressLabel.AutoSize = true;
-            this.uxProgressLabel.Location = new System.Drawing.Point(124, 96);
+            this.uxProgressLabel.Location = new System.Drawing.Point(143, 96);
             this.uxProgressLabel.Name = "uxProgressLabel";
             this.uxProgressLabel.Size = new System.Drawing.Size(24, 13);
             this.uxProgressLabel.TabIndex = 6;
@@ -112,11 +114,31 @@
             this.uxUseFolderSchema.Text = "Use Folder Schema";
             this.uxUseFolderSchema.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Source:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Destination:";
+            // 
             // UserInterface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.ClientSize = new System.Drawing.Size(334, 161);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.uxUseFolderSchema);
             this.Controls.Add(this.uxProgressLabel);
             this.Controls.Add(this.uxDest);
@@ -126,8 +148,8 @@
             this.Controls.Add(this.uxProgressBar);
             this.Controls.Add(this.uxMergeButton);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(300, 200);
-            this.MinimumSize = new System.Drawing.Size(300, 200);
+            this.MaximumSize = new System.Drawing.Size(350, 200);
+            this.MinimumSize = new System.Drawing.Size(350, 200);
             this.Name = "UserInterface";
             this.Text = "Pdf Merge Tool";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UserInterface_FormClosing);
@@ -148,6 +170,8 @@
         private System.Windows.Forms.FolderBrowserDialog uxDestBrowser;
         private System.Windows.Forms.Label uxProgressLabel;
         private System.Windows.Forms.CheckBox uxUseFolderSchema;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
